38cbcf0ead1c5419559bac9c5179d5f1c7934de8
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-05-29 21:06:38 -0700

    nonupdate

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
     useUnifiedTopology: true,
     useFindAndModify: false,
 });
-
+ 
 app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
